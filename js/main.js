e323b556d5dd5bccb3264bacd18ef0aeffa24e49
James Cearnal <james.cearnal@gmail.com>
2024-09-29 14:54:00 -0400

    Worked on service worker

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
+// Initialize Materialize form select
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
-  });
+});
+
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/serviceworker.js')
+        .then((reg) => {
+          console.log('Service Worker Registered', reg);
+        })
+        .catch((err) => {
+          console.error('Service Worker Registration Failed', err);
+        });
+    });
+  }
   
